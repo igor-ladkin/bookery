dbe4752a0080fec01cebb9d2d9fae1b63ce40615
Igor Ladkin <dgkplan@googlemail.com>
2022-06-09 19:02:34 +0200

    Add payment processing mock

--- app/models/payment.rb
+++ app/models/payment.rb
@@ -1,4 +1,27 @@
 class Payment < ApplicationRecord
+  Payment::ChargeError = Class.new StandardError
+
+  class SuccessAdapter
+    def process(booking, user)
+      Payment.create! booking: booking, user: user, state: :completed
+    end
+  end
+
+  class FailureAdapter
+    def process(booking, user)
+      Payment.create! booking: booking, user: user, state: :pending
+      raise Payment::ChargeError
+    end
+  end
+
+  cattr_accessor :adapter
+
+  self.adapter = SuccessAdapter.new
+
+  def self.process(...)
+    adapter.process(...)
+  end
+
   belongs_to :user
   belongs_to :booking
 
