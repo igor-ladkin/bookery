5c9579ad70d7dd3a725bd8a8d864ffd29a660af3
Igor Ladkin <dgkplan@googlemail.com>
2022-06-09 21:19:19 +0200

    Rely on payment to define if booking is paid

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,8 +1,12 @@
 class Booking < ApplicationRecord
   belongs_to :user
   belongs_to :concert
+  has_one :payment
 
   validates :ticket_type, inclusion: { in: ->(booking) { booking.concert&.available_ticket_types || [] } }
   validates :quantity, numericality: { greater_than_or_equal_to: 0 }
 
+  def paid?
+    payment.present? && payment.completed?
+  end
 end
