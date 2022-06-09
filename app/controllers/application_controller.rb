975b0a2408b1c270e7b38e91dfa33aa2beb23942
Igor Ladkin <dgkplan@googlemail.com>
2022-06-09 16:31:36 +0200

    Set dummy user as current one

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,11 @@
 class ApplicationController < ActionController::Base
+  before_action :set_current_user
+
+  attr_accessor :current_user
+
+  private
+
+  def set_current_user
+    self.current_user = User.first
+  end
 end
