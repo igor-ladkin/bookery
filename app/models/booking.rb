aca9513325d11960c929ddb571c389e745c1060d
Igor Ladkin <dgkplan@googlemail.com>
2022-06-09 21:42:03 +0200

    Do not allow to buy more than 5 tickets

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,10 +1,15 @@
 class Booking < ApplicationRecord
+  TICKET_LIMIT = 5
+
   belongs_to :user
   belongs_to :concert
   has_one :payment
 
   validates :ticket_type, inclusion: { in: ->(booking) { booking.concert&.available_ticket_types || [] } }
-  validates :quantity, numericality: { greater_than_or_equal_to: 0 }
+  validates :quantity, numericality: {
+    greater_than_or_equal_to: 0,
+    less_than_or_equal_to: TICKET_LIMIT
+  }
 
   def paid?
     payment.present? && payment.completed?
