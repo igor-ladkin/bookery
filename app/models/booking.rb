2f64d672c3a0e2a6dd7c290e670e2af9511a7aaa
Igor Ladkin <dgkplan@googlemail.com>
2022-06-09 17:07:24 +0200

    Restrict booking ticket types to available on concert

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -2,7 +2,7 @@ class Booking < ApplicationRecord
   belongs_to :user
   belongs_to :concert
 
-  validates :ticket_type, presence: true
+  validates :ticket_type, inclusion: { in: ->(booking) { booking.concert&.available_ticket_types || [] } }
   validates :quantity, numericality: { greater_than_or_equal_to: 0 }
 
   enum state: {
--- spec/models/booking_spec.rb
+++ spec/models/booking_spec.rb
@@ -2,19 +2,32 @@ require 'rails_helper'
 
 RSpec.describe Booking, type: :model do
   describe "validations" do
+    let(:concert) { Concert.find_by! title: "Imagine Dragons" }
+
     it "is not valid with empty ticket_type" do
-      booking = described_class.new(ticket_type: "").tap(&:valid?)
+      booking = build_booking ticket_type: ""
+      expect(booking.errors).to have_key(:ticket_type)
+    end
+
+    it "is not valid with non-existing ticket_type" do
+      booking = build_booking ticket_type: "premium", concert: concert
       expect(booking.errors).to have_key(:ticket_type)
     end
 
     it "is not valid with empty quantity" do
-      booking = described_class.new(quantity: "").tap(&:valid?)
+      booking = build_booking quantity: ""
       expect(booking.errors).to have_key(:quantity)
     end
 
     it "is not valid with quantity < 0" do
-      booking = described_class.new(quantity: -1).tap(&:valid?)
+      booking = build_booking quantity: -1
       expect(booking.errors).to have_key(:quantity)
     end
+  end
+
+  private
+
+  def build_booking(params)
+    described_class.new(params).tap(&:valid?)
   end
 end
