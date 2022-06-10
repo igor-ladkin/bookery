14e33e09d3cb25fcd91fc64a355a9d734a516864
Igor Ladkin <dgkplan@googlemail.com>
2022-06-10 15:27:49 +0200

    Update seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,16 +1,20 @@
+include ActionView::Helpers::AssetUrlHelper
+
 Payment.delete_all
 Booking.delete_all
 Concert.delete_all
 User.delete_all
 
-
-alex = User.create! name: "alex"
+alex  = User.create! name: "alex"
 sasho = User.create! name: "sasho"
-igor = User.create! name: "igor"
+igor  = User.create! name: "igor"
 
-imagine_dragons = Concert.create! title: "Imagine Dragons", starts_at: 1.day.from_now, sales_open_at: "2022-07-10 13:00:00", remaining_ticket_count: 1000, available_ticket_types: ["standard", "vip"]
-kendrick_lamar = Concert.create! title: "Kendrick Lamar", starts_at: 1.day.from_now, sales_open_at: "2020-07-07 19:00:00", remaining_ticket_count: 500, available_ticket_types: ["standard", "premium"]
-adele = Concert.create! title: "Adele", starts_at: 1.day.from_now, sales_open_at: "2020-07-05 15:00:00", remaining_ticket_count: 298
+imagine_dragons = Concert.create! title: "Imagine Dragons", starts_at: 1.day.from_now, sales_open_at: "2022-07-10 13:00:00", remaining_ticket_count: 1000, available_ticket_types: ["standard", "vip"], image_url: image_url("imagine-dragons.jpeg")
+kendrick_lamar  = Concert.create! title: "Kendrick Lamar", starts_at: 1.day.from_now, sales_open_at: "2020-07-07 19:00:00", remaining_ticket_count: 1337, available_ticket_types: ["standard", "premium"], image_url: image_url("kendrick-lamar.jpeg")
+adele           = Concert.create! title: "Adele", starts_at: 1.day.from_now, sales_open_at: "2020-07-05 15:00:00", remaining_ticket_count: 298, image_url: image_url("adele.jpeg")
+u2              = Concert.create! title: "U2", starts_at: 1.day.from_now, sales_open_at: "2020-07-05 15:00:00", remaining_ticket_count: 500, available_ticket_types: ["premium", "vip"], image_url: image_url("u2.jpeg")
+foo_fighters    = Concert.create! title: "Foo Fighters", starts_at: 1.day.from_now, sales_open_at: "2020-07-05 15:00:00", remaining_ticket_count: 3000, image_url: image_url("foo-fighters.jpeg")
+coldplay        = Concert.create! title: "Coldplay", starts_at: 1.day.from_now, sales_open_at: "2020-07-05 15:00:00", remaining_ticket_count: 420, available_ticket_types: ["premium"], image_url: image_url("coldplay.jpeg")
 
 booking = Booking.create! user: sasho, concert: adele, ticket_type: "standard", quantity: 2
 
