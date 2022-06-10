9aef5f0e876e1f21df1e7d1285e322b6690cc224
Igor Ladkin <dgkplan@googlemail.com>
2022-06-10 15:51:53 +0200

    Allow hijacking user session by passing query sting

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,18 @@
 class ApplicationController < ActionController::Base
+  before_action :hijack_user_session
   before_action :set_current_user
 
   attr_accessor :current_user
 
   private
 
+  def hijack_user_session
+    if params[:u].present?
+      session[:user_name] = params[:u]
+    end
+  end
+
   def set_current_user
-    self.current_user = User.first
+    self.current_user = User.find_by(name: session[:user_name]) || User.first
   end
 end
