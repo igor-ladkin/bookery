4792de36e15ce9675c50a75127dbff51d9d96d87
Igor Ladkin <dgkplan@googlemail.com>
2022-06-10 11:42:32 +0200

    Rename supported ticket types

--- app/models/concert.rb
+++ app/models/concert.rb
@@ -1,14 +1,12 @@
 class Concert < ApplicationRecord
+  SUPPORTED_TICKET_TYPES = %w[standard premium vip]
+
   serialize :available_ticket_types, Array
 
   validates :title, :starts_at, :sales_open_at, presence: true
   validates :remaining_ticket_count, numericality: { greater_than_or_equal_to: 0 }
   validates :available_ticket_types, presence: true
   validate :validate_available_ticket_types
-
-  def self.available_ticket_types
-    %w[standard premium vip]
-  end
 
   def sold_out?
     remaining_ticket_count.zero?
@@ -17,8 +15,8 @@ class Concert < ApplicationRecord
   private
 
   def validate_available_ticket_types
-    unless available_ticket_types.all? { |type| Concert.available_ticket_types.include?(type) }
-      errors.add(:available_ticket_types, "must be one of #{Concert.available_ticket_types.join(', ')}")
+    unless available_ticket_types.all? { _1.in? SUPPORTED_TICKET_TYPES }
+      errors.add(:available_ticket_types, "must be one of #{SUPPORTED_TICKET_TYPES.join(', ')}")
     end
   end
 end
